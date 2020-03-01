fea4b2d0870cc36f5eefd426af8093d1e82ef215
kesavamoorthy-code <kesavamoorthy.nagaraj@cognizant.com>
2020-03-01 13:31:11 -0500

    Assignment 10 - Spring cloud developer - Debugging added

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -10,9 +10,12 @@ public class ProjectClient {
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
+        System.out.println("Allocation control : 1.3" + restOperations );
+        System.out.println("Allocation control : 1.4" + registrationServerEndpoint );
     }
 
     public ProjectInfo getProject(long projectId) {
+        System.out.println("Allocation control : 1.5" + restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class) );
         return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
     }
 }
